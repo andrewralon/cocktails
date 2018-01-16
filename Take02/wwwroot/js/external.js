99e6e93c75cdf8204b1dd26d64201c6727652bc2
Nathan Landis <nathanlandis@gmail.com>
2018-01-16 15:13:02 -0600

    Handle numerical ordering in new ingredient addition as well

--- Take02/wwwroot/js/external.js
+++ Take02/wwwroot/js/external.js
@@ -5,6 +5,22 @@
         url: url,
         method: "GET"
     }).success(function (partialView) {
-        $('#newIngredient').append(partialView);
+        // Set the sort order based on existing items on the page
+        var maxNumber = 0;
+        $('.ingredientsContainer').find('input.number').each(function() {
+            console.log($(this).val());
+            var number = parseInt($(this).val());
+            if(number > maxNumber) {
+                maxNumber = number;
+            }
+        });
+
+        // See Recipes/Create.cshtml, we need to wrap the response so it's movable
+        var wrapper = $('<div class="ingredientWrapper"></div>').append(partialView);
+        $('.ingredientsContainer').append(wrapper);
+        $(wrapper).find('input.number').val(maxNumber + 1);
+
+        wireOrderingLinks();
+        wireRemoveLinks();
     });
 }
