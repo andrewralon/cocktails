3d5dec8bb4d7eb33308dac5abba0f039923e9639
Nathan Landis <nathanlandis@gmail.com>
2018-01-16 18:53:02 -0600

    COC-9 Cleanup home page and add some bling

--- Take02/Controllers/HomeController.cs
+++ Take02/Controllers/HomeController.cs
@@ -4,29 +4,56 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Take02.Models;
+using Take02.ViewModels;
 
 namespace Take02.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly CocktailsContext _context;
+
+        public HomeController(CocktailsContext context)
         {
-            return View();
+            _context = context;
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> Index()
         {
-            ViewData["Message"] = "Your application description page.";
+            var mostRecipes = await _context
+            .Recipe
+            .GroupBy(a => a.LibraryId)
+            .Select(a => new 
+            {
+                LibraryId = a.Key,
+                Count = a.Count()
+            })
+            .OrderByDescending(a => a.Count)
+            .Take(5)
+            .ToListAsync();
 
-            return View();
-        }
+            var libraryIds = mostRecipes.Select(a => a.LibraryId);
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+            var libraries = await _context.Library
+            .Where(a => libraryIds.Contains(a.Id))
+            .ToListAsync();
 
-            return View();
+            var topLibraries = from summary in mostRecipes
+                               join library in libraries on summary.LibraryId equals library.Id
+                               select new LibrarySummaryViewModel
+                               {
+                                   LibraryId = library.Id,
+                                   LibraryName = library.Name,
+                                   RecipeCount = summary.Count
+                               };
+
+            var model = new HomeViewModel
+            {
+                TopLibraries = topLibraries.ToList()
+            };
+
+            return View(model);
         }
 
         public IActionResult Error()
